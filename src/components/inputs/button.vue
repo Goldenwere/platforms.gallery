298fed22240d8abbebe9f24926b664bd8ee54701
Lightling <contact@lightling.xyz>
2023-08-31 23:38:41 -0400

    create button

---
+++ src/components/inputs/button.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineEmits<{
+  (e: 'click', event: Event): void
+}>()
+</script>
+
+<template lang="pug">
+button.gw-input.gw-input-container(
+  @click='$emit("click", $event)'
+)
+  slot
+</template>
+
+<style scoped lang="sass">
+@import 'src/styles/inputs.scss'
+</style>
